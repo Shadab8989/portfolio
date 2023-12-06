5f69e63ffc6919219e3de1de0fa09aacc56db529
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2023-12-06 11:05:10 +0530

    unwanted files removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import Navbar from "./components/navbar/navbar.js"
 import Home from "./components/Home/home.js"
-import About from "./components/About/about"
 import Contact from "./components/Contact/contact.js"
 import Projects from "./components/Projects/projects.js"
-import Certificates from "./components/certificates/certificates"
-import Resume from "./components/Resume/resume.js"
 
 function App() {
   return (
@@ -13,11 +10,8 @@ function App() {
     <Navbar />
     <span id='HOME' className='anchor'></span>
     <Home />
-    {/* <Resume /> */}
     <span id='PROJECTS' className='anchor'></span>
     <Projects />
-    {/* <About /> */}
-    {/* <Certificates /> */}
     <Contact />
     </div>
   );
