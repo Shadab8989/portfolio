24d48a45894e3cccc33ad3d04c3a47b5a57c985a
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2023-12-28 11:22:22 +0530

    changes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -16,7 +16,7 @@ const Home = () => {
 							rel="noopener noreferrer"
 						>
 							<button>
-								Resume{" "}
+								Resume
 								<img
 									src={process.env.PUBLIC_URL + "/images/download-icon.png"}
 									alt="download"
